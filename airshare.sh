4ad17a93f9efdc0bff570712b5b8436ec2de3690
HasinduLanka <hlrathnayaka@gmail.com>
2021-02-21 19:53:02 +0530

    -y

--- airshare.sh
+++ airshare.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-sudo apt install wget git screen
+sudo apt install -y wget git screen
 git clone --single-branch --branch linux64sc-install --depth 1 https://github.com/Bitblazers-lk/AirShareRelease
 cd AirShareRelease 
 sudo ./install-linux.sh
+cd ..
